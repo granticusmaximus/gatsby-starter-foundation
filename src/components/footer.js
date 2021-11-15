2fe1be62b97f18f4680788572c65b4a7085aa1d1
Grant Watson <gwatson117@gmail.com>
2021-11-14 22:10:48 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,14 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">Grant Watson</Link>
+        <span>
+        info@grantwatson.dev
+        </span>
       </p>
     </div>
   </footer>
