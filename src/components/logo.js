aabde889a39ff1b1a416718cce842d232ed1d58a
Grant Watson <gwatson117@gmail.com>
2021-11-16 19:11:52 -0700

    Update logo.js

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import logo from "../../public/assets/logo.png"
 
 const Logo = props => (
   <div className="site-logo">
-    <Link to="/">{props.title}</Link>
+    <Link to="/">{logo}{" "}{props.title}</Link>
   </div>
 )
 
